ba241b75883ac79956b4a6d2d5bb8cafa7c0a91b
Verda Korzeniewski <verdakor@gmail.com>
2023-07-27 14:21:01 -0400

    Add click handler to ItemList component

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
 import ItemListComponent from './components/ItemListComponent.vue'
 import ItemInterface from './components/items/Item.ts'
+import { reactive } from 'vue'
 
-const items: ItemInterface[]= [
+const items = reactive([
   { id: 1,
     name: "Item 1",
     selected: false },
@@ -12,12 +13,22 @@ const items: ItemInterface[]= [
   { id: 3,
     name: "Item 3",
     selected: false }
-];
+])
+
+const onSelectItem = (id: number) => {
+  const item = items.find(o => o.id === id)
+  if(!item) {
+    console.warn(`onSelectItem: could not find item with id: {$ id }`)
+    return
+  }
+  item.selected = !item.selected
+  console.log('onSelectItem', item.id, item.selected)
+}
 </script>
 
 <template>
   <div class="home">
-    <ItemListComponent :items="items"/>
+    <ItemListComponent :items="items" @selectItem="onSelectItem"/>
   </div>
 </template>
 
--- src/components/ItemListComponent.vue
+++ src/components/ItemListComponent.vue
@@ -4,15 +4,24 @@ import type { ItemInterface } from '../../components/items/Item.interface'
 
 defineProps<{ items: ItemInterface }>()
 
+const emit = defineEmits<{
+  (e: 'selectItem', id: number): any
+}>()
+
 const count = ref(0)
+
+const handleClick = (item: ItemInterface) => {
+  item.selected = !item.selected
+  console.log('handleItemClick', item.id, item.selected)
+}
 </script>
 
 <template>
   <div class="card">
     <h3>Items:</h3>
     <ul>
-      <li v-for="(item, index) in items" :key="item.id">
-        {{ item.name }}
+      <li v-for="(item, index) in items" :key="item.id" @click="handleClick(item)">
+        {{ item.name }} [{{ item.selected }}]
       </li>
     </ul>
   </div>
