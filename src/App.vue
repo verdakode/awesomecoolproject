77dd42dd28096d32f5f9b8b394dc8e02d05ae25b
Verda Korzeniewski <verdakor@gmail.com>
2023-07-27 12:02:34 -0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+
+const items: any[]= [
+  { id: 1,
+    name: "Item 1" },
+  { id: 2,
+    name: "Item 2" },
+  { id: 3,
+    name: "Item 3" }
+];
+</script>
+
+<template>
+  <div class="home">
+    <HelloWorld :items="items"/>
+  </div>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{ items: any }>()
+
+const count = ref(0)
+</script>
+
+<template>
+  <div class="card">
+    <h3>Items:</h3>
+    <ul>
+      <li v-for="(item, index) in items" :key="item.id">
+        {{ item.name }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+</style>
